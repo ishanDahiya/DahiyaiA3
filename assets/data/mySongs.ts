e154e124e6de7ff3a359a59cc15e61050e720617
ishanDahiya <52500042+ishanDahiya@users.noreply.github.com>
2019-07-24 23:56:02 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,8 @@
+import { Songs } from '../../app/songinfo';
+
+export const MYSONGS: Songs[] = [
+    {sname:'I dont care',sartist:'Ed Sheeran',sgenre:'Electropop',sreleased:'10 May,2019',spicture:'./images/img1.jpg'},
+    {sname:'Bad Liar',sartist:'Imagine Dragons',sgenre:'Electropop',sreleased:'November 6.2018',spicture:'./images/img2.jpg'},
+    {sname:'The Search',sartist:'NF',sgenre:'Hip Hop',sreleased:'July 26,2019',spicture:'./images/img3.jpg'},
+    {sname:'Wow',sartist:'Post Malone',sgenre:'Hip Hop',sreleased:'December 24.2018',spicture:'./images/img4.png'}
+]
